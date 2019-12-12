67a2b1dbe409e5493098dc9e1ff499930dcf2597
Celtor123 <celsosan11@gmail.com>
2019-12-12 23:53:03 +0100

    Importación de anko y prueba Mensajes.

--- app/src/main/java/com/example/reto/MainActivity.kt
+++ app/src/main/java/com/example/reto/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.reto
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import org.jetbrains.anko.toast
+
 
 class MainActivity : AppCompatActivity() {
     private var internet: Button? =null
@@ -12,26 +14,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        toast("Bienvenido, Elija el reto")
     }
 
     override fun onStart() {
         super.onStart()
         internet= findViewById(R.id.btnint)
-        otro= findViewById(R.id.bottom)
+        otro= findViewById(R.id.btnotr)
         mate= findViewById(R.id.btnmte)
         imagen= findViewById(R.id.btmima)
         internet?.setOnClickListener{
-
+             toast("Ha elegido Internet")
         }
         otro?.setOnClickListener {
-
+            toast("Ha elegido el OTRO")
         }
         mate?.setOnClickListener {
-
+            toast("Ha elegido mate")
         }
         imagen?.setOnClickListener {
-
+            toast("Ha elegido crear una imagen")
         }
     }
 }
